a02bed06fb4762844b61eb0eca3a407fb72bd265
Pablo Teixeira <pablopst@gmail.com>
2019-12-03 21:21:38 -0200

    99% finish

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,39 +1,38 @@
 import React, { Component } from 'react';
-// import api from '../../service/api';
 
 import Logo from '../../assets/farmbox-logo.svg';
-import '../../styles/header.css'
+import './header.css'
 import { FaCog, FaUser } from 'react-icons/fa';
 
 
 export default class Header extends Component {
     render() {
-        // const {report} = this.props;
         return (
             <header>
                 <div className="center">
                     <section>
                         <div className="logo-operation">
-                            <img src={Logo}></img>
+                            <a href="/"><img src={Logo} alt="logo"></img></a>
                             <span className="operation">Operação</span>
                         </div>
 
                         <div className="nav-link">
                             <nav>
                                 <ul>
-                                    <li><a href='#'>Mapa</a></li>
-                                    <li><a href='#'>Agenda</a></li>
-                                    <li><a href='#'>Monitoramentos</a></li>
-                                    <li><a href='#'>Aplicações</a></li>
-                                    <li><a href='#'>Insumos</a></li>
-                                    <li><a href='#'>Pluviometria</a></li>
-                                    <li><a href='#'>Semeadura</a></li>
+                                    <li><a href='/'>Mapa</a></li>
+                                    <li><a href='/'>Agenda</a></li>
+                                    <li><a href='/'>Monitoramentos</a></li>
+                                    <li><a href='/'>Aplicações</a></li>
+                                    <li><a href='/'>Insumos</a></li>
+                                    <li><a href='/'>Pluviometria</a></li>
+                                    <li><a href='/'>Semeadura</a></li>
                                 </ul>
                             </nav>
 
                             <div className="icons">
-                                <FaCog color="#a7aaac" size={19}/>
-                                <FaUser color="#a7aaac" size={19}/>
+                                <a href='/'><FaCog color="#a7aaac" size={19}/></a>
+                                <a href='/'><FaUser color="#a7aaac" size={19}/></a>
+                          
                             </div>
                         </div>
                         
